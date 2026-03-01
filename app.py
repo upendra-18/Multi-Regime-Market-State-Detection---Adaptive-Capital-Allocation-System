7050ad21cb516ee4fbc03c1db2078abd21847a8b
upendra-18 <upendrareddy637@gmail.com>
2026-03-01 10:43:33 +0530

    re deploy

--- app.py
+++ app.py
@@ -183,6 +183,7 @@ def health():
     }
 
 
+
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host="0.0.0.0", port=port)
